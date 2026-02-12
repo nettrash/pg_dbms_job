a9f21656d097d8cf3570b0d8f2dc1c5a110c8616
nettrash <nettrash@nettrash.me>
2026-02-12 19:26:41 +0100

    v1.5.2

--- rust/src/logging.rs
+++ rust/src/logging.rs
@@ -35,11 +35,9 @@ pub fn dprint(config: &Config, level: &str, msg: &str) {
 
     if !fname.is_empty() {
         if let Ok(mut out) = OpenOptions::new().append(true).create(true).open(&fname) {
-            unsafe { libc::flock(out.as_raw_fd(), libc::LOCK_EX) };
             let _ = out
                 .write_all(format!("{t} [{}]: {level}: {msg}\n", process::id()).as_bytes())
                 .and_then(|_| out.flush());
-            unsafe { libc::flock(out.as_raw_fd(), libc::LOCK_UN) };
         } else {
             eprintln!("ERROR: can't write to log file {fname}");
             eprintln!("{t} [{}]: {level}:  {msg}", process::id());
